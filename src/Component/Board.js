440fab3c9740f068d4af2c13165c2171fa2c810a
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-11-24 15:21:16 +0100

    add player steps

--- src/Component/Board.js
+++ src/Component/Board.js
@@ -4,35 +4,47 @@ import Cercle from '../Utilities/Cercle'
 import Triangle from '../Utilities/Triangle'
 import Rectangle from '../Utilities/rectangle'
 
-
+const Shapes = [{ id: 1, item: <Cercle />,name:'cer' }, { id: 2, item: <Triangle />,name:'tri' }, { id: 3, item: <Rectangle />,name:'rec' }]
 const Board = () => {
     const [openedCard, setOpenedCard] = useState([]);
     const [matched, setMatched] = useState([]);
-
-    const Shapes = [{ id: 1, item: <Cercle /> }, { id: 2, item: <Triangle /> }, { id: 3, item: <Rectangle /> }]
-
+    const [steps,setSteps]=useState(0)
+   
+   
+    
     //currently there are 4 Shapes but we need the pair
 
     const pairOfPokemons = [...Shapes, ...Shapes];
-
+   
     function flipCard(index) {
+        
         setOpenedCard((opened) => [...opened, index]);
+        setSteps(steps+1)
+        console.log(steps)
 
     }
 
     useEffect(() => {
+       
         if (openedCard < 2) return;
 
         const firstMatched = pairOfPokemons[openedCard[0]];
         const secondMatched = pairOfPokemons[openedCard[1]];
 
         if (secondMatched && firstMatched.id === secondMatched.id) {
+           
             setMatched([...matched, firstMatched.id]);
+         
         }
 
         if (openedCard.length === 2) setTimeout(() => setOpenedCard([]), 1000);
+      
     }, [openedCard]);
-
+    const restartGame =()=>{
+        setMatched([])
+        setSteps(0)
+        
+    }
     return (
         <div className="App">
             <h1>Technical Test Shipzzer</h1>
@@ -62,7 +74,9 @@ const Board = () => {
                     );
                 })}
             </div>
-            <button>Restart Game</button>
+            <div> 
+                <h6> Steps: {steps}</h6>
+            <button onClick={e=>restartGame()}>Restart Game</button></div>
         </div>)
 
 }
