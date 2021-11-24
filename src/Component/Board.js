2beccfd46a452a16c0fb4425b61e245546abff37
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-11-24 16:08:19 +0100

    add timer to the game

--- src/Component/Board.js
+++ src/Component/Board.js
@@ -4,46 +4,59 @@ import Cercle from '../Utilities/Cercle'
 import Triangle from '../Utilities/Triangle'
 import Rectangle from '../Utilities/rectangle'
 
-const Shapes = [{ id: 1, item: <Cercle />,name:'cer' }, { id: 2, item: <Triangle />,name:'tri' }, { id: 3, item: <Rectangle />,name:'rec' }]
+const Shapes = [{ id: 1, item: <Cercle />, name: 'cer' }, { id: 2, item: <Triangle />, name: 'tri' }, { id: 3, item: <Rectangle />, name: 'rec' }]
 const Board = () => {
     const [openedCard, setOpenedCard] = useState([]);
     const [matched, setMatched] = useState([]);
-    const [steps,setSteps]=useState(0)
-   
-   
-    
+    const [steps, setSteps] = useState(0)
+    const [seconds, setSeconds] = useState(0);
+    const [timer,setTimer]=useState(null)
+    const [hide,setHide]=useState(true)
+
     //currently there are 4 Shapes but we need the pair
 
     const pairOfPokemons = [...Shapes, ...Shapes];
-   
+
     function flipCard(index) {
-        
+
         setOpenedCard((opened) => [...opened, index]);
-        setSteps(steps+1)
-        console.log(steps)
+        setSteps(steps + 1)
 
     }
 
-    useEffect(() => {
+    const startCounting = () => {
+        setHide(false)
+        const interval =setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+        }, 1000);
+         setTimer(interval) 
        
+    }
+
+   
+
+    useEffect(() => {
+        
         if (openedCard < 2) return;
 
         const firstMatched = pairOfPokemons[openedCard[0]];
         const secondMatched = pairOfPokemons[openedCard[1]];
 
         if (secondMatched && firstMatched.id === secondMatched.id) {
-           
+
             setMatched([...matched, firstMatched.id]);
-         
+
         }
 
         if (openedCard.length === 2) setTimeout(() => setOpenedCard([]), 1000);
-      
+
     }, [openedCard]);
-    const restartGame =()=>{
+    const restartGame = () => {
         setMatched([])
         setSteps(0)
-        
+        clearInterval(timer);
+        setSeconds(0)
+        startCounting()
     }
     return (
         <div className="App">
@@ -61,6 +74,7 @@ const Board = () => {
                             className={`pokemon-card ${isFlipped ? "flipped" : ""} `}
                             key={index}
                             onClick={() => flipCard(index, shape)}
+                            hidden={hide}
                         >
                             <div className="inner">
                                 <div className="front">
@@ -74,9 +88,11 @@ const Board = () => {
                     );
                 })}
             </div>
-            <div> 
-                <h6> Steps: {steps}</h6>
-            <button onClick={e=>restartGame()}>Restart Game</button></div>
+            <div>
+                <h6>  Steps: {steps}</h6>
+                <h6> time:{seconds}</h6>
+                <button hidden={!hide}  onClick={e => startCounting()}>Start Game</button>
+                <button hidden={hide}  onClick={e => restartGame()}>Restart Game</button></div>
         </div>)
 
 }
